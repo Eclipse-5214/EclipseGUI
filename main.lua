5ad1b11627fa32f348f02c2df67850450b9d5339
Eclipse-5214 <83417472+Eclipse-5214@users.noreply.github.com>
2022-08-17 11:26:53 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,33 +11,7 @@ MainSection:NewKeybind("Toggle UI", "Toggles the UI", Enum.KeyCode.RightShift, f
 end)
 
 MainSection:NewButton("Anti Afk", "no kicks", function()
-    MainSection:UpdateSection("Sucsess")
-    wait(1)
-    print("Anti Afk Loaded")
-    local GC = getconnections or get_signal_cons
-    if GC then
-        for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
-            if v["Disable"] then
-                v["Disable"](v)
-            elseif v["Disconnect"] then
-                v["Disconnect"](v)
-            end
-        end
-    else
-        print("lol bad exploit")
-        local vu = game:GetService("VirtualUser")
-        game:GetService("Players").LocalPlayer.Idled:connect(function()
-            vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-            wait(1)
-            vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-        end)
-    end
-
-    local VirtualUser=game:service'VirtualUser'
-    game:service'Players'.LocalPlayer.Idled:connect(function()
-    VirtualUser:CaptureController()
-    VirtualUser:ClickButton2(Vector2.new())
-    end)
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Proxylol/OtherScripts/main/AntiAfk.lua'),true))()
 end)
 
 --Petsim--
